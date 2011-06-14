584790d35d376b3da5d276ae760b533a01da161c
Peter Johnson <peter@tortall.net>
2011-06-13 19:58:30 -0700

    Start adding inlines for person editor.

--- admin.py
+++ admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from roster.models import *
 
+class PersonPhoneInline(admin.StackedInline):
+    model = PersonPhone
+    extra = 1
+
+class PersonEmailInline(admin.StackedInline):
+    model = PersonEmail
+    extra = 1
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [PersonPhoneInline, PersonEmailInline]
+    list_filter = ['status', 'lead', 'programs']
+
 admin.site.register(Organization)
 admin.site.register(Program)
 admin.site.register(School)
@@ -8,9 +20,9 @@ admin.site.register(Company)
 admin.site.register(Email)
 admin.site.register(Address)
 admin.site.register(Phone)
-admin.site.register(Person)
-admin.site.register(PersonEmail)
-admin.site.register(PersonPhone)
+admin.site.register(Person, PersonAdmin)
+#admin.site.register(PersonEmail)
+#admin.site.register(PersonPhone)
 admin.site.register(Waiver)
 admin.site.register(Adult)
 admin.site.register(Student)
